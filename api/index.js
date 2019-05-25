406da3c5a446bf6de132b40eb2a3f3dd1fecb522
Vikas Verma <mail@vikas.cc>
2019-05-25 02:35:13 -0700

    Dynamic loading of API frameworks,. just drop in the API package either in directory directly or via Docker and attaching volume with API to easily load up a new version of API

--- api/index.js
+++ api/index.js
@@ -3,10 +3,13 @@ var router = express.Router();
 var fs = require('fs');
 var path = require('path');
 
+// Get the available API versions or supported versions from Environment Variable
 var api_versions = (process.env.API_VERSIONS || 'beta,v1,v2').split(',');
 
 api_versions.map(api => {
 
+  // Check for valid API package which has modules.json
+  // modules.json has individual modules to be loaded from API package
   if (fs.existsSync(path.join(__dirname, api, `modules.json`)))
     var modules = require(path.join(__dirname, api, `modules.json`));
 
@@ -14,6 +17,8 @@ api_versions.map(api => {
 
   if (modules !== undefined) {
     modules.map(m => {
+      // try to load individual module and also see if the module has "routes" defined in it
+      // If routes, exists then load them up dynamically using eval 
       let module = require(path.join(__dirname, api, m));
       if (module !== undefined && module.routes !== undefined) {
         module.routes.map(r => {
