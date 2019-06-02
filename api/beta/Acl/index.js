0dcc3a7dc51370889c34e32c1062e5e751d34567
Vikas Verma <mail@vikas.cc>
2019-06-02 04:58:40 -0700

    refactored the api loader

--- api/beta/Acl/index.js
+++ api/beta/Acl/index.js
@@ -1,74 +1,44 @@
+    module.exports = (PreProcess, PostProcess) => {
+        return {
+            DeleteAclRule: function (req, res, next) {
+                PreProcess(req, res, "DeleteAclRule");
+                res.send('TODO');
+                PostProcess(req, res, "DeleteAclRule");
+            },
 
-const PrepareResponse = function (req,res,caller) {
-    res.set('X-Params', Buffer.from(JSON.stringify(req.params)).toString());
-    res.set('X-Method', Buffer.from(JSON.stringify(caller)).toString());
-    
-};
+            GetAclRule: function (req, res, next) {
+                PreProcess(req, res, "GetAclRule");
+                res.send('TODO');
+                PostProcess(req, res, "GetAclRule");
+            },
+            CreateAclRule: function (req, res, next) {
+                PreProcess(req, res, "CreateAclRule");
+                res.send('TODO');
+                PostProcess(req, res, "CreateAclRule");
+            },
 
-this.DeleteAclRule = function (req, res, next) {
-    PrepareResponse(req, res,"DeleteAclRule");
-    res.status(200).send();
-};
-this.GetAclRule = function (req, res, next) {
-    PrepareResponse(req, res, "GetAclRule");
-    res.send('TODO');
-};
-this.CreateAclRule = function (req, res, next) {
-    PrepareResponse(req, res, "CreateAclRule");
-    res.send('TODO');
-};
+            GetAclList: function (req, res, next) {
+                PreProcess(req, res, "GetAclList");
+                res.send('TODO');
+                PostProcess(req, res, "GetAclList");
+            },
+            UpdateAclRule: function (req, res, next) {
+                PreProcess(req, res, "UpdateAclRule");
+                res.send('TODO');
+                PostProcess(req, res, "UpdateAclRule");
+            },
+            ReplaceAclRule: function (req, res, next) {
+                PreProcess(req, res, "ReplaceAclRule");
+                res.send('TODO');
+                PostProcess(req, res, "ReplaceAclRule");
+            },
+            WatchAclList: function (req, res, next) {
+                PreProcess(req, res, "WatchAclList");
+                res.send('TODO');
+                PostProcess(req, res, "WatchAclList");
+            },
 
-this.GetAclList = function (req, res, next) {
-    PrepareResponse(req, res, "GetAclList");
-    res.send('TODO');
-};
-this.UpdateAclRule = function (req, res, next) {
-    PrepareResponse(req, res, "UpdateAclRule");
-    res.send('TODO');
-};
-this.ReplaceAclRule = function (req, res, next) {
-    PrepareResponse(req, res, "ReplaceAclRule");
-    res.send('TODO');
-};
-this.WatchAclList = function (req, res, next) {
-    PrepareResponse(req, res, "WatchAclList");
-    res.send('TODO');
-};
 
-this.routes = [
-    {
-        Path: "/calendars/:calendarId/acl/:ruleId",
-        Function: this.GetAclRule,
-        Method: "get"
-    },
-    {
-        Path: "/calendars/:calendarId/acl/:ruleId",
-        Function: this.DeleteAclRule,
-        Method: "delete"
-    },
-    {
-        Path: "/calendars/:calendarId/acl/:ruleId",
-        Function: this.UpdateAclRule,
-        Method: "patch"
-    },
-    {
-        Path: "/calendars/:calendarId/acl/:ruleId",
-        Function: this.ReplaceAclRule,
-        Method: "put"
-    },
-    {
-        Path: "/calendars/:calendarId/acl/",
-        Function: this.CreateAclRule,
-        Method: "post"
-    },
-    {
-        Path: "/calendars/:calendarId/acl/",
-        Function: this.GetAclList,
-        Method: "get"
-    },
-    {
-        Path: "/calendars/:calendarId/acl/watch",
-        Function: this.WatchAclList,
-        Method: "post"
+
+        }
     }
-];
--- api/index.js
+++ api/index.js
@@ -6,6 +6,16 @@ var path = require('path');
 // Get the available API versions or supported versions from Environment Variable
 var api_versions = (process.env.API_VERSIONS || 'beta,v1,v2').split(',');
 
+let PreProcess = function (req, res, caller) {
+  res.set('X-Params', Buffer.from(JSON.stringify(req.params)).toString());
+  res.set('X-Method', Buffer.from(JSON.stringify(caller)).toString());
+  res.set('X-StartTime', Buffer.from(JSON.stringify(new Date().toUTCString())).toString());
+};
+
+let PostProcess = function (req, res, caller) {
+ 
+};
+
 api_versions.map(api => {
 
   // Check for valid API package which has modules.json
@@ -16,16 +26,20 @@ api_versions.map(api => {
   var routeMap = [];
 
   if (modules !== undefined) {
-    modules.map(m => {
+
+    Object.keys(modules).map(moduleName => {
       // try to load individual module and also see if the module has "routes" defined in it
       // If routes, exists then load them up dynamically using eval 
-      let module = require(path.join(__dirname, api, m));
-      if (module !== undefined && module.routes !== undefined) {
-        module.routes.map(r => {
-          eval(`router.${r.Method}(${"'/' + api + r.Path"},r.Function);`)
-        })
-      }
+      //console.log("Loading module ", moduleName)
+      let Module = require(path.join(__dirname, api, moduleName));
 
+      if (typeof Module === "function") {
+        let module = Module(PreProcess,PostProcess);
+        modules[moduleName].map(route => {
+          eval(`router.${route.Method}(${"'/' + api + route.Path"},eval('module.' + route.Function));`)
+        });
+        ``
+      }
     });
   }
 
