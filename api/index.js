07a0d20549a1e80ef134522c3cc0a31957f5cc0a
Vikas Verma <mail@vikas.cc>
2020-08-22 23:08:21 -0700

    some changes...

--- api/index.js
+++ api/index.js
@@ -2,18 +2,34 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var path = require('path');
+const {
+  performance//,
+  // PerformanceObserver
+} = require('perf_hooks');
 
 // Get the available API versions or supported versions from Environment Variable
 var api_versions = (process.env.API_VERSIONS || 'beta,v1,v2').split(',');
 
 let PreProcess = function (req, res, caller) {
-  res.set('X-Params', Buffer.from(JSON.stringify(req.params)).toString());
-  res.set('X-Method', Buffer.from(JSON.stringify(caller)).toString());
-  res.set('X-StartTime', Buffer.from(JSON.stringify(new Date().toUTCString())).toString());
+  if(!res.headersSent){
+    res.append('X-Params', Buffer.from(JSON.stringify(req.params)).toString());
+    res.append('X-Method', Buffer.from(JSON.stringify(caller)).toString());
+    
+    // Capture the request start time
+    res.locals.startTime=performance.now()
+  }
 };
 
-let PostProcess = function (req, res, caller) {
- 
+let PostProcess = function (req, res, caller,buffer) {
+  res.locals.endTime=performance.now()
+  res.locals.duration=`${Number.parseFloat((res.locals.endTime-res.locals.startTime)).toPrecision(4)} Milliseconds`
+
+  if(!res.headersSent){
+    if(res.locals.startTime)
+      res.append('X-Duration', Buffer.from(res.locals.duration));    
+  }
+  
+  res.send(res.locals.duration)
 };
 
 api_versions.map(api => {
